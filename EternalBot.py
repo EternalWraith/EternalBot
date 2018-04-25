61a33245268d58efbe4934b6b36b073b6c3d62ca
jacob lawrence <jajay2000@live.com>
2018-04-26 00:42:49 +0100

    Update

--- EternalBot.py
+++ EternalBot.py
@@ -7,6 +7,7 @@ __version__="0.0.0"
 command_prefix='e!'
 description = 'Enter a description here'
 bot = commands.Bot(command_prefix)
+client = discord.Client()
 
 @bot.event
 async def on_ready():
@@ -14,9 +15,12 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    client.change_presence(activity="Nibbling on cookies", status=discord.Status.dnd)
 
 @bot.command(pass_context=True)
 async def ping(ctx):
-    await ctx.send("Pong")
+    await ctx.send("Pong!")
+
+
 
 bot.run('NDM4Nzc1Mjc1MDE1MTEwNjY2.DcJtxw.8U1TRxkhnLEkRHvqF8YcWztUeGc')
