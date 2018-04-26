16d06b625eb6fc3b6d128a5ec41e69409d8c20d2
jacob lawrence <jajay2000@live.com>
2018-04-26 02:45:33 +0100

    Added more commands, and made bot more cute

--- EternalBot.py
+++ EternalBot.py
@@ -4,10 +4,9 @@ from discord.ext import commands
 
 __version__="0.0.0"
 
-command_prefix='e!'
+prefix="e!"
 description = 'Enter a description here'
-bot = commands.Bot(command_prefix)
-client = discord.Client()
+bot = commands.Bot(prefix)
 
 @bot.event
 async def on_ready():
@@ -15,12 +14,37 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(activity=discord.Game(name="Nibbling on Cookies", url="http://twitch.tv/megaskull100", type=1), status=discord.Status.dnd)
+    await bot.change_presence(activity=discord.Game(name="Nibbling on Cookies", type=1, url="http://twitch.tv/megaskull100"))
 
-@bot.command(pass_context=True)
+@bot.event
+async def on_message(message):
+    if message.content.count("<@{0.user.id}>".format(bot)) > 0:
+        await message.channel.send("Baka! Why are you pinging me {0.author.mention}?! Can't you see I am trying to eat cookies? >:(".format(message))
+
+    if message.content.startswith("e!test"):
+        await message.channel.send(str(message.channel.id))
+
+@bot.command(brief="Ping me, I'll Pong you right back! >:3")
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send("Pong! *(psst, stats for the nerds! {0}, {1})*".format(ctx.channel,ctx.channel.id))
 
 
+"""
 
+"""
+
+#Bestest's stuff
+@bot.command(brief='Cyka Blayt Bratukha! Rush B {-}7')
+async def communism(ctx):
+        await ctx.channel.send('☭')
+        await ctx.message.delete()
+
+@bot.command(brief="Hey! Baka! Why you looking at other people's profile pictures? Hmmm?")
+async def pfp(ctx, *, user: discord.User):
+        em = discord.Embed()
+        em.set_image(url=user.avatar_url)
+        await ctx.channel.send(embed=em)
+
+
+#DO NOT REMOVE THIS, EVERYTHING MUST BE ABOVE THIS
 bot.run('NDM4Nzc1Mjc1MDE1MTEwNjY2.DcJtxw.8U1TRxkhnLEkRHvqF8YcWztUeGc')
