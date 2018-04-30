1d3e8b420a23fa94e69d880b53f3dd112f5255b1
jacob lawrence <jajay2000@live.com>
2018-04-30 01:17:43 +0100

    Added options command

--- EternalBot.py
+++ EternalBot.py
@@ -394,5 +394,32 @@ async def withdraw(ctx, *, text: str="All"):
     conn.commit()
     conn.close()
 
+@bot.command(name='settings')
+async def settings(ctx, *, command: str, text: str, value: int):
+    editable = { "work" : 0 }
+    editable["work"] = { "Location" : DBNAME, "Table" : "server_{0}".format(ctx.guild.id), "Where": False }
+    editable["work"]["Options"] = {"max": {"ID": "MaxWork"}, "min": {"ID": "MinWork"}, "wait": {"ID": "WorkCooldown"} }
+    editable["work"]["Options"]["max"]["Message"] = "Set the max payout for working to {0}".format(value)
+    editable["work"]["Options"]["min"]["Message"] = "Set the min payout for working to {0}".format(value)
+    editable["work"]["Options"]["wait"]["Message"] = "Set the waiting time between working to {0} minutes".format(value)
+    if command in editable:
+        if text in editable[command]["Options"]:
+            conn = database.connect(editable[command]["Location"])
+            config = conn.cursor()
+            if editable[command]["Where"]:
+                pass
+            else:
+                config.execute("""
+UPDATE {0} SET {1} = {2}
+""".format(editable[command]["Table"],editable[command]["Options"][text]["ID"],value))
+            conn.commit()
+            conn.close()
+            upload(editable[command]["Location"])
+            await ctx.send(editable[command]["Options"][text]["Message"])
+        else:
+            await ctx.send("{0} is not an available option for {1}".format(text,command))
+    else:
+        await ctx.send("{0} is not an editable command".format(command))
+
 #DO NOT REMOVE THIS, EVERYTHING MUST BE ABOVE THIS
 bot.run('NDM4Nzc1Mjc1MDE1MTEwNjY2.DcJtxw.8U1TRxkhnLEkRHvqF8YcWztUeGc')
