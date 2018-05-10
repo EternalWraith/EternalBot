cddb315590e124ada92996979c540631acb295d2
EternalWraith <38735189+EternalWraith@users.noreply.github.com>
2018-05-10 10:10:53 +0100

    Update EternalCurrency.py

--- EternalCurrency.py
+++ EternalCurrency.py
@@ -3,6 +3,7 @@ import sqlite3 as database
 
 from discord.ext import commands
 from EternalCloudStorage import *
+from EternalChecks import *
 
 DBNAME = "config.db"
 
@@ -117,6 +118,43 @@ class Currency:
         await ctx.send("You withdrew {0}!".format(amount))
         conn.commit()
         conn.close()
+	
+	@commands.command(name='addmoney', brief="Add's money to someones account. Requires Manage Channel permission")
+    @bot_control()
+    async def work(self, ctx, *, person: discord.User, amount: int, place: str="Bank"):
+        global upload
+ 
+        conn = database.connect("{0}.db".format(ctx.guild.id))
+        config = conn.cursor()
+        config.execute("SELECT Wallet, Bank FROM Money WHERE UserID={0}".format(person.id))
+        money = config.fetchall()
+
+        if len(money) > 0:
+         	wallet = money[0][0]
+			bank = money[0][1]
+			if place == "Bank":
+				add_to = bank+amount
+			else:
+				place = "Wallet"
+				add_to = wallet+amount
+			config.execute("""
+        UPDATE Money SET {2} = {1} WHERE UserID = {0}
+        """.format(ctx.author.id, add_to, place))
+			await ctx.send("<@{0}> was given :cookie:{1}!".format(person.id,add_to))
+        else:
+			if place == "Bank": 
+				add_bank = amount
+				add_wallet = 0
+			else:
+				add_bank = 0
+				add_wallet = amount
+            config.execute("""
+        INSERT INTO Money (UserID, Wallet, Bank, LastWork) VALUES ({0}, {2}, {1}, "{3}");
+        """.format(ctx.author.id, add_bank, add_wallet, "Never"))
+            await ctx.send("<@{0}> was given :cookie:{1}!".format(person.id,add_to))
+        conn.commit()
+        conn.close()
+        upload("{0}.db".format(ctx.guild.id))
 
 
 def setup(bot):
