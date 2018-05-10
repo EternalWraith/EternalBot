49309f8383a0d61bb537da487b732f11fdd00c36
EternalWraith <38735189+EternalWraith@users.noreply.github.com>
2018-05-10 09:51:54 +0100

    Update EternalChecks.py

--- EternalChecks.py
+++ EternalChecks.py
@@ -5,5 +5,5 @@ from discord.ext import commands
 
 def bot_control():
     async def predicate(ctx):
-        return ctx.author.guild_permissions.administrator or ctx.author.permissions_in(ctx.channel.id).manage_channels
+        return ctx.author.guild_permissions.administrator or ctx.author.guild_permissions.manage_channels
     return commands.check(predicate)
